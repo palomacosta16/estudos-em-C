1b8745d96d7e583affda47610c69553ebb302bef
Paloma Costa <97255085+palomacosta16@users.noreply.github.com>
2022-06-22 18:25:47 -0300

    Add files via upload

---
+++ Atv01ex02.c
@@ -0,0 +1,20 @@
+//sintese
+//	objetivo " "
+//	entrada ""
+//	saida ""
+#include <stdio.h>
+
+int main () {
+	float sb, sl, x, y ;
+	printf ("Salario Bruto:");
+	scanf ("%f", &sb);
+	
+	x = (sb * 0.9);
+	y = (x * 0.95);
+	
+	printf ("Salario liquido: %f", y);
+	
+	return 0;
+	
+}
+	
