add8c4fd9df22800736e6f1b39549ecffad2bd53
Paloma Costa <97255085+palomacosta16@users.noreply.github.com>
2022-06-22 18:26:17 -0300

    Add files via upload

---
+++ atv01ex3.c
@@ -0,0 +1,27 @@
+//sintese
+//	objetivo " "
+//	entrada ""
+//	saida ""
+#include <stdio.h>
+
+int main (void) {
+	float peso, altura, media;
+	
+	printf ("Digite seu peso:");
+	scanf ("%f", &peso); 
+	
+	printf("digite sua altura:") ;
+	scanf("%f", &altura);
+	
+	media = (altura - 1.10) * 100;
+
+	printf ("Seu peso ideal e: %f" , media);
+		
+	return 0;
+}
+		
+	
+	
+	
+	
+	
